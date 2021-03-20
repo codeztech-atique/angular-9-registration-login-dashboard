154534d18b966ae84081759048a44b8b735b7d16
codeztech-atique <atique1224@gmail.com>
2021-03-20 21:52:41 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-9-registration-login-dashboard
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-9-registration-login-dashboard)
---
+++ src/environment.ts
@@ -0,0 +1,4 @@
+// workaround environment config to get example working in stackblitz
+export const environment = {
+    apiUrl: 'http://localhost:4000'
+};
