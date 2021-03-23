cdd9a85f71f2537cde4b53e4840b14015ea634db
codeztech-atique <atique1224@gmail.com>
2021-03-23 23:39:21 +0530

    3 files modified, 1 added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,177 @@
-import { Component } from '@angular/core';
-
-import { User } from '../_models';
-import { AccountService } from '../_services';
-
-@Component({ templateUrl: 'home.component.html' })
+import { User } from "../_models";
+import { AccountService } from "../_services";
+import { Component, Output, EventEmitter, ViewChild } from "@angular/core";
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  FormControl
+} from "@angular/forms";
+import { HttpClient, HttpParams } from "@angular/common/http";
+@Component({
+  templateUrl: "home.component.html",
+  styleUrls: ["./home.component.css"]
+})
 export class HomeComponent {
-    user: User;
+  user: User;
+  private clickTimeout = null;
+  private updateTimeout = null;
+  userName: any;
+  userMobile: any;
+  tableData: any = [];
+  showEditTable: boolean = false;
+  editRowID: any = "";
 
-    constructor(private accountService: AccountService) {
-        this.user = this.accountService.userValue;
+  todoForm = this.formBuilder.group({
+    name: new FormControl("", [Validators.required]),
+    mobile: new FormControl("", [Validators.required])
+  });
+
+  constructor(
+    private accountService: AccountService,
+    private http: HttpClient,
+    private formBuilder: FormBuilder
+  ) {
+    this.user = this.accountService.userValue;
+  }
+
+  public setClickTimeout(callback) {
+    // clear any existing timeout
+    clearTimeout(this.clickTimeout);
+    this.clickTimeout = setTimeout(() => {
+      this.clickTimeout = null;
+      callback();
+    }, 2000);
+  }
+
+  submitData() {
+    if (this.clickTimeout) {
+      this.setClickTimeout(() => {});
+    } else {
+      // if timeout doesn't exist, we know it's first click
+      // treat as single click until further notice
+      this.setClickTimeout(itemId => this.handleSingleClick());
     }
+  }
+  public handleSingleClick() {
+    // 2 sec - deboucing time
+    console.log(this.userName, this.userMobile);
+    var dataSet = {
+      name: this.userName,
+      mobile: this.userMobile
+    };
+    // this.shared.createTodos(dataSet).subscribe(
+    //   data => {
+    //     const res = JSON.parse(JSON.stringify(data));
+    //     console.log(res);
+    //     if (res) {
+    //       var filterData = {};
+    //       var parseJson = res.data;
+    //       filterData["name"] = parseJson.name;
+    //       filterData["mobile"] = parseJson.mobile;
+    //       filterData["id"] = parseJson.id;
+    //       var showMessage = <HTMLElement>document.getElementById("success");
+    //       showMessage.style.cursor = "block";
+
+    //       this.ngOnInit();
+    //     }
+    //   },
+    //   error => {
+    //     var showMessage = <HTMLElement>document.getElementById("success");
+    //     showMessage.style.cursor = "none";
+    //     var showErMessage = <HTMLElement>document.getElementById("error");
+    //     showErMessage.style.cursor = "block";
+    //     console.log(error);
+    //   }
+    // );
+  }
+
+  deleteSingleTodoItems(data) {
+    console.log(data);
+    if (confirm("Are you sure you want to delete this ?")) {
+      // Save it!
+      console.log(data + " delete items from database.");
+      // this.shared.deleteSingleTodos(data).subscribe(
+      //   data => {
+      //     const res = JSON.parse(JSON.stringify(data));
+      //     if (res) {
+      //       console.log(res);
+      //       this.ngOnInit();
+      //     }
+      //   },
+      //   error => {
+      //     console.log(error);
+      //   }
+      // );
+    } else {
+      // Do nothing!
+      console.log("Thing was not saved to the database.");
+    }
+  }
+
+  ngOnInit() {
+    this.tableData = [];
+    // this.shared.getAllTodos().subscribe(
+    //   data => {
+    //     const res = JSON.parse(JSON.stringify(data));
+    //     if (res) {
+    //       var parseJson = res.data;
+    //       for (let i = 0; i < parseJson.length; i++) {
+    //         this.tableData.push({
+    //           id: parseJson[i].id,
+    //           name: parseJson[i].name,
+    //           mobile: parseJson[i].mobile
+    //         });
+    //       }
+    //     }
+    //   },
+    //   error => {
+    //     console.log(error);
+    //   }
+    // );
+  }
+  Edit(val) {
+    this.editRowID = val;
+  }
+
+  onKey(id, name, mobile) {
+    if (this.clickTimeout) {
+      this.setUpdateTimeout(() => {});
+    } else {
+      // if timeout doesn't exist, we know it's first click
+      // treat as single click until further notice
+      this.setUpdateTimeout(itemId => this.updateSingleClick(id, name, mobile));
+    }
+  }
+  updateSingleClick(id, name, mobile) {
+    var resD = {
+      id: id,
+      name: name,
+      mobile: mobile
+    };
+    console.log(resD);
+    // this.shared.updateSingleTodos(resD).subscribe(
+    //   data => {
+    //     const res = JSON.parse(JSON.stringify(data));
+    //     if (res) {
+    //       alert(
+    //         "UserId:" + id + "\n" + "Name:" + name + " Successfully Updated !!!"
+    //       );
+    //       this.ngOnInit();
+    //     }
+    //   },
+    //   error => {
+    //     console.log(error);
+    //   }
+    // );
+  }
+
+  public setUpdateTimeout(callback) {
+    // clear any existing timeout
+    clearTimeout(this.updateTimeout);
+    this.updateTimeout = setTimeout(() => {
+      this.updateTimeout = null;
+      callback();
+    }, 2000);
+  }
 }
--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -1,22 +1,14 @@
-import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { CommonModule } from "@angular/common";
 
-import { UsersRoutingModule } from './users-routing.module';
-import { LayoutComponent } from './layout.component';
-import { ListComponent } from './list.component';
-import { AddEditComponent } from './add-edit.component';
+import { UsersRoutingModule } from "./users-routing.module";
+import { LayoutComponent } from "./layout.component";
+import { ListComponent } from "./list.component";
+import { AddEditComponent } from "./add-edit.component";
 
 @NgModule({
-    imports: [
-        CommonModule,
-        ReactiveFormsModule,
-        UsersRoutingModule
-    ],
-    declarations: [
-        LayoutComponent,
-        ListComponent,
-        AddEditComponent
-    ]
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, UsersRoutingModule],
+  declarations: [LayoutComponent, ListComponent, AddEditComponent]
 })
-export class UsersModule { }
+export class UsersModule {}
